1d9a05073b69021acadf560d62c35450f3155e4a
Samuel Yin <wsgddjy@live.com>
2024-02-25 04:30:13 +1000

    234. Handling HTTP Errors

--- src/components/AvailablePlaces.tsx
+++ src/components/AvailablePlaces.tsx
@@ -1,22 +1,43 @@
 import Places from "./Places.tsx";
 import { useEffect, useState } from "react";
+import Error from "./Error.tsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFecthing, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
   // use useEffect to stop the infinite loop
   useEffect(() => {
     // use async and await to fetch the data, it will wait for the data to be fetched.
     // this can be done with .then() as well, but async and await is more readable.
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch("http://localhost:3000/placesss");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch places.");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places. Please try again later.",
+        });
+      }
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+  if (error) {
+    return (
+      <Error
+        title="An error occured!"
+        message={error.message}
+        onConfirm={undefined}
+      />
+    );
+  }
 
   return (
     <Places
