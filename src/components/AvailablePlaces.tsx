d6460941c0a505dee689ae8d0830ed657670c692
Samuel Yin <wsgddjy@live.com>
2024-02-25 03:51:30 +1000

    232. Using async / await

--- src/components/AvailablePlaces.tsx
+++ src/components/AvailablePlaces.tsx
@@ -5,13 +5,14 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   // use useEffect to stop the infinite loop
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((res) => {
-        return res.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    // use async and await to fetch the data, it will wait for the data to be fetched.
+    // this can be done with .then() as well, but async and await is more readable.
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+    fetchPlaces();
   }, []);
 
   return (
