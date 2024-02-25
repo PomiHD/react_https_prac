1edab7d91d2fdc9a4b4551382f5e29b65482fdb7
Samuel Yin <wsgddjy@live.com>
2024-02-26 00:02:55 +1000

    237. Sending Data with POST Requests

--- src/http.ts
+++ src/http.ts
@@ -9,3 +9,20 @@ export async function fetchAvailablePlaces() {
   }
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch(`http://localhost:3000/user-places`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // the body must follow the same structure as the server expects
+    body: JSON.stringify({ places: places }),
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to update user data.");
+  }
+  return resData.message;
+}
